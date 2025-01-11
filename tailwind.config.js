6f79c4167dea30a976360b51bcb8cceb4c3f7135
jayceh <jaycehold@gmail.com>
2025-01-11 15:55:31 -0700

    Changed color scheme, work on other pages

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        bg_h:'#1d2021',
+        bg:'#282828',
+        bg_s: '#32302f',
+        bg1:  '#3c3836',
+        bg2:  '#504945',
+        bg3:  '#665c54',
+        bg4:  '#7c6f64',
+      
+        fg:  '#fbf1c7',
+        fg1: '#ebdbb2',
+        fg2: '#d5c4a1',
+        fg3: '#bdae93',
+        fg4: '#a89984',
+      
+        red:    '#fb4934',
+        green:  '#b8bb26',
+        yellow: '#fabd2f',
+        blue:   '#83a598',
+        purple: '#d3869b',
+        aqua:   '#8ec07c',
+        gray:   '#928374',
+        orange: '#fe8019',
+      
+        red_dim:    '#cc2412',
+        green_dim:  '#98971a',
+        yellow_dim: '#d79921',
+        blue_dim:   '#458588',
+        purple_dim: '#b16286',
+        aqua_dim:   '#689d6a',
+        gray_dim:   '#a89984',
+        orange_dim: '#d65d0e'
+      },
+    },
+  },
+};
